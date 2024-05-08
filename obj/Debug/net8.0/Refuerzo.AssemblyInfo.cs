1417d5e79625acb15876c64f7175494971c7bb62
Kimy474 <kimberlyduran474@gmail.com>
2024-05-07 22:10:47 -0400

    completo

--- obj/Debug/net8.0/Refuerzo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Refuerzo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Refuerzo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36df1d474595d3dbf5a112fbd0015c20e3043278")]
 [assembly: System.Reflection.AssemblyProductAttribute("Refuerzo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Refuerzo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
